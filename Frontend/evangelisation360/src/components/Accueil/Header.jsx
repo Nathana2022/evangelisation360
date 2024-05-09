fcdc3a3309017b81ef44bd46e0a3052f1253a153
Nathana2022 <nathanaeldon495@gmail.com>
2024-05-09 16:19:03 +0100

    Gestion du header

--- Frontend/evangelisation360/src/components/Accueil/Header.jsx
+++ Frontend/evangelisation360/src/components/Accueil/Header.jsx
@@ -7,25 +7,26 @@ import SignUpForm from "../signUpForm/SignUpForm";
 
 const Header = () => {
   return (
-    <header className="w-1519,2 h-136,6 ml-189.6 mr-189.6 pl-15 pr-15 ">
-      <div className="bg-[#f4f4f4] w-1519,2 h-50 text-white flex justify-center items-center space-x-4">
-        <span className="text-base text-teal-500 font-open-sans font-sans">
+    <header className="">
+      <div className="bg-[#f4f4f4] flex justify-center justify-around">
+        <p className="text-base text-teal-500 font-open-sans font-sans">
           +243 (0) 82 80 500 18
-        </span>
-        <span className="text-base text-teal-500 font-open-sans font-sans">
+        </p>
+        <p className="text-base text-teal-500 font-open-sans font-sans">
           info@360Evangelisation.com
-        </span>
-        <span className="text-base text-teal-500 font-open-sans font-sans">
+        </p>
+        <p className="text-base text-teal-500 font-open-sans font-sans">
           www.360Evangelisation.com
-        </span>
+        </p>
       </div>
-      <div className="flex justify-center items-center z-1001 hover:z-50">
+      <div className="flex justify-center justify-around items-center">
         <div>
-          <img src={LogoApp} alt="Logo" className="h-8 mr-4" />
+          <img src={LogoApp} alt="Logo" className="h-auto w-auto" />
         </div>
+
         <div>
           <nav>
-            <ul className="flex text-teal-500">
+            <ul className="flex text-teal-500 justify-center ">
               <li className="ml-4">Fonctionnalités</li>
               <li className="ml-4">Appli pour l'Eglise</li>
               <li className="ml-4">Tarifs</li>
@@ -33,15 +34,16 @@ const Header = () => {
             </ul>
           </nav>
         </div>
-        <div className="flex">
+
+        <div className="flex gap-3">
           <Link to={Login}>
-            <button className="bg-blue-500 text-white px-4 py-2 ml-4">
+            <button className="bg-teal-500 text-white px-4 py-2 ml-4">
               CONNEXION
             </button>
           </Link>
 
           <Link to={SignUpForm}>
-            <button className="bg-blue-500 text-white px-4 py-2 ml-4">
+            <button className="bg-teal-500 text-white px-4 py-2 ml-4">
               INSCRIPTION
             </button>
           </Link>
